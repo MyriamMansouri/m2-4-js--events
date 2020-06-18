32f9def7058d83e76403ab390ac645b2c33175bb
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-18 12:22:29 -0400

    Exercise 1.0

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,13 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+
+const body = document.querySelector("body");
+let text = ""
+const addText = () => {
+    text = text + "Hey you! ";
+    body.innerText = text;
+
+};
+
+document.addEventListener("click", addText)
