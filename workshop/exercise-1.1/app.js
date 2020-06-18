1b6d8916b03e9be8f73a7897cc5a87a186b1764f
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-18 12:51:02 -0400

    Exercise 1.1 - Warmer

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,19 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+
+let body = document.querySelector("body");
+let isClicked = false;
+
+const handleClick = () => {
+    isClicked = true;
+    console.log("clciked")
+};
+
+body.addEventListener("click", handleClick);
+
+const endTheGame = () => {
+    const alertMessage = isClicked === true ? "You won." : "He won.";
+    body.innerHTML = `<p>${alertMessage}</>`
+};
+setTimeout(endTheGame, 1000)
